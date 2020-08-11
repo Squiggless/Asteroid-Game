f118d2c2b9cd399a7bebe90fc845e42d32bf3f50
Squiggless <quinnwaldron@icloud.com>
2020-08-11 19:17:12 +0000

    added the player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 
 from pygame.locals import *
 
+from player import Player
 
 pygame.init()
 
@@ -17,10 +18,14 @@ background_rect = background_image.get_rect()
 background_image = pygame.transform.scale(background_image, (int(0.5 * background_rect.width), int(0.5 * background_rect.height)))
 background_rect = background_image.get_rect()
 background_rect.center = (width // 2, height // 2)
+
+player = Player(image_name = "space_ship.png", scale = 0.28)
+
 def main():
 	while True:
 		clock.tick(60)
 		screen.blit(background_image, background_rect)
+		screen.blit(player.image, player.rect)
 		pygame.display.flip()
 
 main()
--- player.py
+++ player.py
@@ -1,10 +1,10 @@
 import pygame
 
-class Player(pygame_sprite.Sprite):
+class Player(pygame.sprite.Sprite):
 	def __init__(self, image_name, scale):
 		self.image = pygame.image.load(image_name)
 		self.rect = self.image.get_rect()
 
-		self.image = pygame.transform.scale(self.image,(int(scale*self.rect.width), int(scale*self.rect.heigt)))
+		self.image = pygame.transform.scale(self.image,(int(scale*self.rect.width), int(scale*self.rect.height)))
 
 		self.rect = self.image.get_rect()
