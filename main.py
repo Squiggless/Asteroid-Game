f5115c5eb2e0fe23d5de1e01fd644111129448c7
Squiggless <quinnwaldron@icloud.com>
2020-08-12 17:16:57 +0000

    made it move

--- main.py
+++ main.py
@@ -19,11 +19,23 @@ background_image = pygame.transform.scale(background_image, (int(0.5 * backgroun
 background_rect = background_image.get_rect()
 background_rect.center = (width // 2, height // 2)
 
-player = Player(image_name = "space_ship.png", scale = 0.28)
+player = Player(image_name = "space_ship.png", scale = 0.28, pos = (400, 530))
 
 def main():
 	while True:
 		clock.tick(60)
+
+		for event in pygame.event.get():
+				if event.type == KEYDOWN:
+					if event.key == K_DOWN:
+						player.speed[1] = 10
+					if event.key == K_UP:
+						player.speed[1] = -10
+					if event.key == K_LEFT:
+						player.speed[0] = -10
+					if event.key == K_RIGHT:
+						player.speed[0] = 10
+		player.move_player()
 		screen.blit(background_image, background_rect)
 		screen.blit(player.image, player.rect)
 		pygame.display.flip()
--- player.py
+++ player.py
@@ -1,10 +1,18 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, image_name, scale):
+	def __init__(self, image_name, scale, pos):
 		self.image = pygame.image.load(image_name)
 		self.rect = self.image.get_rect()
 
 		self.image = pygame.transform.scale(self.image,(int(scale*self.rect.width), int(scale*self.rect.height)))
 
 		self.rect = self.image.get_rect()
+		self.reset(pos)
+		self.speed = pygame.math.Vector2(0, 0)
+	
+	def reset(self, pos):
+		self.rect.center = pos
+
+	def move_player(self):
+		self.rect.move_ip(self.speed)
